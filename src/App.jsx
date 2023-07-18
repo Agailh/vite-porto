30ede7ab80a41d307ebe95f1d9850fa1da8aad26
Agailh <agailham86@gmail.com>
2023-07-18 13:18:58 +0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Sidenav from './Components/Sidenav'
+
+function App() {
+  
+
+  return (
+    <>
+      <Sidenav />
+    </>
+  )
+}
+
+export default App
