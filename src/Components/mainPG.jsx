f61ffa3001256378dd7994461a877c067d4c5950
Agailh <agailham86@gmail.com>
2023-07-18 15:58:17 +0700

    commit

--- src/Components/mainPG.jsx
+++ src/Components/mainPG.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const mainPG = () => {
   return (
     <div id='main'>
-        <img src="/" alt="/" />
-
+        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="/" alt="/" />
+        <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+         <div>
+            <h1>Hi iam Aga ilham </h1>
+            <h2>im a front end dev</h2>
+         </div>
+        </div>
     </div>
   )
 }
