d924f165b5657fa48bdbfae28090ae2611ddc522
Agailh <agailham86@gmail.com>
2023-07-24 15:55:11 +0700

    add data

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,8 +1,31 @@
 // import React from 'react'
+import Workitem from "./Workitem"
 
+const data = [
+    {
+        year : 2019,
+        title : 'frelance',
+        duration : '6 months',
+        details : 'Dolore non in ut irure tempor esse tempor ullamco incididunt est. Et ipsum labore nisi mollit eu sunt. Ut cupidatat officia dolore dolor mollit pariatur ad eiusmod aute incididunt pariatur. Sint anim cupidatat duis occaecat tempor culpa irure duis laborum do exercitation. Aliqua culpa deserunt voluptate sit cupidatat et et ipsum ut aliquip elit. In non reprehenderit aute aliquip reprehenderit elit veniam ad aliquip minim deserunt tempor do. Aliqua fugiat ea pariatur in reprehenderit velit ex duis anim.'
+    },
+    {
+        year : 2019,
+        title : 'frelance',
+        duration : '6 months',
+        details : 'Dolore non in ut irure tempor esse tempor ullamco incididunt est. Et ipsum labore nisi mollit eu sunt. Ut cupidatat officia dolore dolor mollit pariatur ad eiusmod aute incididunt pariatur. Sint anim cupidatat duis occaecat tempor culpa irure duis laborum do exercitation. Aliqua culpa deserunt voluptate sit cupidatat et et ipsum ut aliquip elit. In non reprehenderit aute aliquip reprehenderit elit veniam ad aliquip minim deserunt tempor do. Aliqua fugiat ea pariatur in reprehenderit velit ex duis anim.'
+    },
+    {
+        year : 2019,
+        title : 'frelance',
+        duration : '6 months',
+        details : 'Dolore non in ut irure tempor esse tempor ullamco incididunt est. Et ipsum labore nisi mollit eu sunt. Ut cupidatat officia dolore dolor mollit pariatur ad eiusmod aute incididunt pariatur. Sint anim cupidatat duis occaecat tempor culpa irure duis laborum do exercitation. Aliqua culpa deserunt voluptate sit cupidatat et et ipsum ut aliquip elit. In non reprehenderit aute aliquip reprehenderit elit veniam ad aliquip minim deserunt tempor do. Aliqua fugiat ea pariatur in reprehenderit velit ex duis anim.'
+    },
+]
 const Work = () => {
   return (
-    <div>Work</div>
+    <div id="work" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+        
+    </div>
   )
 }
 
