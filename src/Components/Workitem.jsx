df2e61a3aa3a6f3f59edbaa9c900ba9e0758c401
Agailh <agailham86@gmail.com>
2023-07-25 13:02:01 +0700

    comit fix error data

--- src/Components/Workitem.jsx
+++ src/Components/Workitem.jsx
@@ -1,9 +1,19 @@
 // import React from 'react'
 
-const Workitem = () => {
+const Workitem = ({year, title , duration, details}) => {
   return (
-    <div>Workitem</div>
-  )
+    <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
+      <li className="mb-10">
+        <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white" />
+        <p className="flex flex-warp gap-4 flex-row items-center justify-start text-xs md:text-sm">
+          <span>{year}</span>
+          <span>{title}</span>
+          <span>{duration}</span>
+        </p>
+        <p>{details}</p>
+      </li>
+    </ol>
+  );
 }
 
 export default Workitem
