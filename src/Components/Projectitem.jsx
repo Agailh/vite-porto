a7410f48c7a277d241edcb175c2c7fa9c83d08cd
Agailh <agailham86@gmail.com>
2023-08-01 13:48:49 +0700

    add project content

--- src/Components/Projectitem.jsx
+++ src/Components/Projectitem.jsx
@@ -1,6 +1,6 @@
 //import React from 'react'
 
-const Projectitem = ({img, title}) => {
+const Projectitem = ({img, title, subtitle}) => {
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-black  group hover:bg-gradient-to-r from-[#242629] to bg-[#16161a] ">
       <div className="py-5 px-5">
@@ -9,10 +9,11 @@ const Projectitem = ({img, title}) => {
             <h3 className="text-2xl font-bold text-white tracking-wider text-center">
                 {title}
             </h3>
-            <p className="pb-4 pt-2 text-white text-center">React JS</p>
-            <a href="/">
+            <p className="pb-4 pt-2 text-white text-center text-sm">{subtitle}</p>
+            {/* <a href="/">
                 <p className="text-center p-3 rounded-lg bg-[#7f5af0] text-[#fffffe] font-bold cursor-pointer text-lg" >More Info</p>
-            </a>
+            </a> */}
+            
         </div>
 
       </div>
